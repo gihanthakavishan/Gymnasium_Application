7f71735b9cd1a99b93462d6f149c2bf321933c39
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-27 22:27:26 +0530

    create hero text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,11 +6,21 @@ const Hero = () => {
     <div className="hero">
         <div className="left-h">
          <Header/>
-
+{/*the ad section*/}
          <div className="the-best-ad">
           <div></div>
           <span>The best partner for your health</span>
          </div>
+{/*hero heading */}
+        <div className="hero-text">
+          <div>
+            <span>Shape Up </span>
+            <span>Your</span>
+          </div> 
+          <div>
+            <span>Healthy Life</span>
+            </div>
+        </div>
         </div>
         <div className="right-h">right hand</div>
     </div>
