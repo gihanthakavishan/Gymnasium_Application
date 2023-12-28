84ad41841dee7a64f8a065c2adb062bee70e1115
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-29 00:12:18 +0530

    create testimonial right side text

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Testimonials.css';
+import { testimonialsData } from '../../data/testimonialsData';
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div className="Testimonials">
+        <div className="left-t">
+         left
+        </div>
+        <div className="right-t">
+            <span>Testimonials</span>
+            <span>What they</span>
+            <span className='stroke-text'>Say about us</span>
+            <span>
+                {testimonialsData[0].review}
+            </span>
+        </div>
+    </div>
   )
 }
 
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -6,20 +6,20 @@ export const testimonialsData = [
   {
     image: image1,
     review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+      "I made the right choice by choosing the Fittyclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'ROY VINCE',
+    status : 'CRICKETER'
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
+    review: 'I have alrady trained more than 100 students since last year, lot of guys were able to shape up their body and level up their strengths',
+    name: 'LIZARD HENRY',
     status: 'COACH'
   },
   {
     image : image3,
     review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
+    name: 'PAUL DE ZUZAN',
     status: "CUSTOMER"
   }
 ];
