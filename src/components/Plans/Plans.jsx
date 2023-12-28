ba4ce80c879334622db205ae9b6e82ee0e869ec0
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-28 22:17:37 +0530

    do plans maping part

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { plansData } from "../../data/plansData";
 import './Plans.css';
 
 const Plans = () => {
   return (
-    <div>Plans</div>
+<div className="plans-container">
+    <div className="programs-header" style={{gap:'3.5rem'}}>
+        <span>LET'S GET</span>
+        <span className='stroke-text'>STARTED</span>
+        <span>WITHUS</span>
+    </div>
+
+    {/*Plans card*/}
+    <div className="plans">
+        {plansData.map((plan, i)=> (
+            <div className="plan" key={i}>
+               {plan.icon}
+               <span>{plan.name}</span>
+               <span>{plan.price}</span>
+                
+                <div className="features">
+                    {plan.features.map((feature, i)=> (
+                        <div className="feature">
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
   )
 }
 
