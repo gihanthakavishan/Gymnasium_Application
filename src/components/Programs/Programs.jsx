d5ddc6fcce3c1fad4d831c645b33ec43d8f57177
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-28 18:23:55 +0530

    changes in programs section

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -8,7 +8,7 @@ const Programs = () => {
         <div className="programs-header">
             <span className='stroke-text'>Explore our</span>
             <span>Programs</span>
-            <span className='stroke-text'>to keep fit you</span>
+            <span className='stroke-text'>to keep fit youself</span>
         </div>
 
         <div className="programs-categories">
@@ -18,7 +18,7 @@ const Programs = () => {
                      <span>{program.heading}</span>
                      <span>{program.details}</span>
                       <div className="join-now">
-                        <span>Join Now</span>
+                        <span>Let's try</span>
                         <img src={RightArrow} alt="" />
                       </div>
                 </div>
