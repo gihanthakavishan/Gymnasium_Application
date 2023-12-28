d4db46017fe136d9ee40aa88a417c13eee09bc85
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-28 14:29:27 +0530

    maping programs data

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import "./Programs.css";
-
+import {programsData} from '../../data/programsData'
 const Programs = () => {
   return (
     <div className="Programs" id="programs">
         <div className="programs-header">
-            <span>Explore our</span>
+            <span className='stroke-text'>Explore our</span>
             <span>Programs</span>
-            <span>to keep fit you</span>
+            <span className='stroke-text'>to keep fit you</span>
+        </div>
+
+        <div className="programs-categories">
+            {programsData.map((program)=>(
+                <div className="category">
+                    {program.image}
+                     <span>{program.heading}</span>
+                     <span>{program.details}</span>
+
+                </div>
+            ))}
         </div>
     </div>
   )
