b8d412cc0922973643c242c3f1bb2c46b79dcf8d
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-28 14:18:06 +0530

    create program text and add css

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import './Programs.css'
+import React from 'react';
+import "./Programs.css";
 
 const Programs = () => {
   return (
     <div className="Programs" id="programs">
-        
+        <div className="programs-header">
+            <span>Explore our</span>
+            <span>Programs</span>
+            <span>to keep fit you</span>
+        </div>
     </div>
   )
 }
