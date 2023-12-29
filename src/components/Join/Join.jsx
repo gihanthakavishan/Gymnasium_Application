49d911e0de0a49d0ece5f8b4e1c0904015e0c260
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-29 14:32:17 +0530

    create join us email field and button

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -8,14 +8,19 @@ const Join = () => {
         <hr />
         <div>
            <span className='stroke-text'>READY TO</span>
-           <span> MASTER YOUR</span>
+           <span>MASTER YOUR</span>
         </div>
         <div>
           <span>FITNESS LEVEL</span>
-          <span className='stroke-text'> WITH US?</span>
+          <span className='stroke-text'>WITH US?</span>
         </div>
       </div>
-      <div className="right-j"></div>
+      <div className="right-j">
+         <form action="" className="contact-container">
+            <input type="email"  name='user-email' placeholder='Enter Your Emailaddress'/>
+            <button className='btn btn-j'>Join Now</button>
+         </form>
+      </div>
     </div>
   )
 }
