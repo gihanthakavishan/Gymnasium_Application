fb332ddbb303434716971e392099bcc554cc1369
gihanthakavishan <gihanthakavishan@gmail.com>
2024-01-01 01:09:46 +0530

    changes in assets

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,22 +1,22 @@
-import image1 from "../assets/t-image1.png";
-import image2 from "../assets/t-image2.jpg";
-import image3 from "../assets/t-image3.jpg";
+import image4 from "../assets/image4.jpg";
+import image5 from "../assets/image5.jpg";
+import image6 from "../assets/image6.jpeg";
 
 export const testimonialsData = [
   {
-    image: image1,
+    image: image4,
     review:"I made the right choice by choosing the Fittyclub and by choosing the right plan and program I already achieved my ideal body!",
     name: 'ROY VINCE',
     status : 'PROFESSIONAL CRICKETER'
   },
   {
-    image: image2,
+    image: image5,
     review: 'I have alrady trained more than 100 students since last year, lot of guys were able to shape up their body and level up the fitness.',
     name: 'LIZARD HENRY',
     status: 'COACH'
   },
   {
-    image : image3,
+    image : image6,
     review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
     name: 'PAUL DE ZUZAN',
     status: "CUSTOMER"
