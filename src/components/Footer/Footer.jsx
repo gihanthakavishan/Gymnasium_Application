621835abe42b35ed539863e0e96c47bc24172e13
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-31 23:59:22 +0530

    add social media links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,17 @@ import Youtube from "../../assets/youtube.jpg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    
+    <div className="Footer-container">
+        <hr />
+        <div className="social-links">
+        <img src={Github} alt="" />
+        <img src={Instagram} alt="" />
+        <img src={LinkedIn} alt="" />
+        <img src={Facebook} alt="" />
+        <img src={Youtube} alt="" />
+        </div>
+    </div>
   )
 }
 
