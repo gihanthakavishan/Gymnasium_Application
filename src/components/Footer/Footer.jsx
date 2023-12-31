6884a2ec687ec31516b79e9f943651f7ec6721df
gihanthakavishan <gihanthakavishan@gmail.com>
2023-12-31 23:42:27 +0530

    import needed images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Footer.css';
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Facebook from "../../assets/facebook.png";
+import Youtube from "../../assets/youtube.jpg";
+
 const Footer = () => {
   return (
     <div>Footer</div>
